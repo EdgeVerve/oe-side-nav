86e179c3a4b8896d5d799f429b8cda980f2b32e8
vamsee <vamseerama_c@infosys.com>
2017-11-24 12:09:40 +0530

    Added CI

--- wct.conf.js
+++ wct.conf.js
@@ -3,15 +3,14 @@ The EdgeVerve proprietary software program ("Program"), is protected by copyrigh
 The Program may contain/reference third party or open source components, the rights to which continue to 
 remain with the applicable third party licensors or the open source community as the case may be and nothing 
 here transfers the rights to the third party and open source components, except as expressly permitted. 
-Any unauthorized reproduction, storage, transmission in any form or by any means (including without limitation to electronic, mechanical, printing, photocopying, recording or  otherwise), or any distribution of this Program,or any portion of it, may result in severe civil and criminal penalties, and will be prosecuted to the maximum extent possible under the law. 
-*/
+Any unauthorized reproduction, storage, transmission in any form or by any means (including without limitation to electronic, mechanical, printing, photocopying, recording or  otherwise), or any distribution of this Program,or any portion of it, may result in severe civil and criminal penalties, and will be prosecuted to the maximum extent possible under the law. */
 
 var browser = process.env.BROWSER || 'chrome';
 var browsers = [];
 browsers.push(browser);
 
 module.exports = {
-  verbose: false,
+  verbose: true,
   testTimeout: 2 * 60 * 1000,
   plugins: {
     sauce: {
@@ -37,7 +36,12 @@ module.exports = {
         '/test/**/*.html',
         '/test/**/*.js',
         '/scripts/*.js'
-      ]
+      ],
+      thresholds: {
+        global: {
+          statements: 65
+        }
+      }
     }
   }
 }
